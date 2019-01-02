3aa7545d7c77c8f18eceed906b705e40c35b60e0
Andrew Rees <andrewtrevor.rees@gmail.com>
2019-01-02 22:22:29 +0000

    adding multiple lists

--- MergeSorting.js
+++ MergeSorting.js
@@ -1,7 +1,82 @@
-var numbers = [6, 2, 1, 7, 3, 5, 4];
+var numbers = [7, 6, 8, 9, 2, 1, 0, 11, -1, 3, 5, 4, 10, 10, 0.5];
+var result = [];
+var lessSoSwapped = 0;
+var moreSoNotSwapped = 0;
+var equalSoNotSwapped = 0;
 
-function splitArray (array) {
-    var splitPoint = Math.floor(array.length / 2)
-    var firstArray = array.slice(0, array)
+function mergeSort() {
+    for (i = 0; i < numbers.length; i += 2) {
+        var array1 = [];
+        array1.push(numbers[i]);
+        var array2 = [];
+        array2.push(numbers[i + 1]);
+        //numbers.splice(numbers[i], 1).splice(numbers[i+1], 1);
+        if (array1[0] < array2[0]) {
+            result.push(array1[0]);
+            result.push(array2[0]);
+            lessSoSwapped++
+        } else if (array1[0] > array2[0]) {
+            result.push(array2[0]);
+            result.push(array1[0]);
+            moreSoNotSwapped++
+        } else if (array1[0] = array2[0]) {
+            result.push(array2[0]);
+            result.push(array1[0]);
+            equalSoNotSwapped++
+        };
+    };
+    numbers = result
+    console.log(numbers)
 }
 
+while (moreSoNotSwapped < numbers.length) {
+    mergeSort()
+}
+
+
+
+
+
+
+
+/*
+
+function splitArray (array) {
+    var splitPoint = Math.floor(numbers.length / 2)
+    var firstArray = numbers.slice(0, splitPoint)
+    var secondArray = numbers.slice(splitPoint)
+    // console.log(firstArray);
+    // console.log(secondArray)
+
+}
+splitArray(numbers);
+
+
+//create arrays for each item in the list
+//to do this, split both of the lists into 2
+//then, split these new lists into 2, etc
+
+
+//from the ol' internet.
+
+function mergeSort (arr) {
+    if (arr.length < 2) {
+      return arr;
+    }
+
+    var mid = Math.floor(arr.length / 2);
+    var subLeft = mergeSort(arr.slice(0, mid));
+    var subRight = mergeSort(arr.slice(mid));
+
+    return merge(subLeft, subRight);
+}
+
+function merge (node1, node2) {
+    var result = [];
+    while (node1.length > 0 && node2.length > 0)
+        result.push(node1[0] < node2[0]? node1.shift() : node2.shift());
+    return result.concat(node1.length? node1 : node2);
+}
+
+console.log(mergeSort(numbers))
+*/
--- SelectionSorting.js
+++ SelectionSorting.js
@@ -1,21 +1,30 @@
 //DONE
 
-var numbers = [7, 6, 8, 9, 2, 1, 0, 11, -1, 3, 5, 4, 10, 10, 0.5];
+const lists = require('./lists.js');
+
+var start = new Date().getTime();
+var smallList = [7, 6, 8, 9, 2, 1, 0, 11, -1, 3, 5, 4, 10, 10, 0.5];        
+var bigList /* 30 */ = [6, 76,49, 51, 15, 16, 39, 72, 6, 91, 99, 79, 9, 65, 62, 0, 97, 35, 51, 20, 91, 86, 37, 28, 66, 93, 76, 98, 69, 32];
+var massiveList /* 100 */ = [57, 60, 0, 20, 24, 74, 92, 85, 37, 60, 48, 34, 47, 36, 59, 27, 59, 95, 40, 70, 36, 21, 67, 7, 3, 95, 64, 22, 84, 49, 61, 90, 41, 95, 22, 73, 95, 99, 6, 86, 37, 64, 33, 69, 54, 82, 22, 32, 77, 98, 37, 76, 40, 40, 26, 13, 63, 24, 65, 31, 34, 53, 48, 66, 49, 36, 79, 99, 56, 81, 81, 20, 50, 80, 5, 55, 65, 47, 97, 87, 39, 61, 80, 49, 60, 71, 86, 67, 52, 58, 15, 19, 48, 8, 50, 66, 37, 68, 67, 85,];  
 var result = [];
-console.log("List at start: " + numbers);
+console.log("List at start: " + massiveList);
 var timesSorted = 0;
 var timesSortingAttempted = 0;
 
-while (numbers.length != 0) {
-    var smallest = Math.min(...numbers);
-    var index = numbers.indexOf(smallest)
-    numbers.splice(index, 1);
+
+while (massiveList.length != 0) {
+    var smallest = Math.min(...massiveList);
+    var index = massiveList.indexOf(smallest)
+    massiveList.splice(index, 1);
     result.push(smallest)
-    //console.log(`Messy list: ${numbers}`)
+    //console.log(`Messy list: ${massiveList}`)
     //console.log(`Sorted list: ${result}`)
     timesSorted++
 };
-numbers = result;
-console.log(`Sorted List at end: ${numbers}`);
+massiveList = result;
+console.log(`Sorted List at end: ${massiveList}`);
 console.log(`Times sorted: ${timesSorted}`);
 console.log(`Times sorting attempted: ${timesSorted + timesSortingAttempted}`);
+var end = new Date().getTime();
+var time = end - start;
+console.log('Execution time: ' + time);
