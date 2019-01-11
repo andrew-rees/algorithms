ab3cbd1b2ef56ab40bd8a7d772c7ba3fedf08293
Andrew Rees <andrewtrevor.rees@gmail.com>
2019-01-11 12:05:07 +0000

    modify selection and lists

--- SelectionSorting.js
+++ SelectionSorting.js
@@ -2,29 +2,53 @@
 
 const lists = require('./lists.js');
 
-var start = new Date().getTime();
-var smallList = [7, 6, 8, 9, 2, 1, 0, 11, -1, 3, 5, 4, 10, 10, 0.5];        
-var bigList /* 30 */ = [6, 76,49, 51, 15, 16, 39, 72, 6, 91, 99, 79, 9, 65, 62, 0, 97, 35, 51, 20, 91, 86, 37, 28, 66, 93, 76, 98, 69, 32];
-var massiveList /* 100 */ = [57, 60, 0, 20, 24, 74, 92, 85, 37, 60, 48, 34, 47, 36, 59, 27, 59, 95, 40, 70, 36, 21, 67, 7, 3, 95, 64, 22, 84, 49, 61, 90, 41, 95, 22, 73, 95, 99, 6, 86, 37, 64, 33, 69, 54, 82, 22, 32, 77, 98, 37, 76, 40, 40, 26, 13, 63, 24, 65, 31, 34, 53, 48, 66, 49, 36, 79, 99, 56, 81, 81, 20, 50, 80, 5, 55, 65, 47, 97, 87, 39, 61, 80, 49, 60, 71, 86, 67, 52, 58, 15, 19, 48, 8, 50, 66, 37, 68, 67, 85,];  
-var result = [];
-console.log("List at start: " + massiveList);
-var timesSorted = 0;
-var timesSortingAttempted = 0;
+var arrayOfLists = [];
+function makeListsForThisSort () {
+    var one = lists.smallList;
+    arrayOfLists.push(one)
+    var two = lists.bigList;
+    arrayOfLists.push(two)
+    var three = lists.massiveList;
+    arrayOfLists.push(three)
+    var four = lists.megaList;
+    arrayOfLists.push(four)
+    var five = lists.galacticList;
+    arrayOfLists.push(five)
+    var six = lists.clusterList;
+    arrayOfLists.push(six)
+}
 
 
-while (massiveList.length != 0) {
-    var smallest = Math.min(...massiveList);
-    var index = massiveList.indexOf(smallest)
-    massiveList.splice(index, 1);
-    result.push(smallest)
-    //console.log(`Messy list: ${massiveList}`)
-    //console.log(`Sorted list: ${result}`)
-    timesSorted++
+function selectionSort(list) {
+    var start = new Date().getTime();
+    var result = [];
+    //console.log(`------------------ ${} ------------------`)
+    //console.log(`List at start: ${list}`);
+    console.log(`Length: ${list.length}`)
+    var timesSorted = 0;
+    var timesSortingAttempted = 0;
+    while (list.length != 0) {
+        var smallest = Math.min(...list);
+        var index = list.indexOf(smallest)
+        list.splice(index, 1);
+        result.push(smallest);
+        timesSorted++;
+    };
+    list = result;
+    //console.log(`Sorted ${listName} at end: ${list}`);
+    console.log(`Times sorted: ${timesSorted}`);
+    console.log(`Times sorting attempted: ${timesSorted + timesSortingAttempted}`);
+    var end = new Date().getTime();
+    var time = end - start;
+    console.log('Execution time: ' + time);
 };
-massiveList = result;
-console.log(`Sorted List at end: ${massiveList}`);
-console.log(`Times sorted: ${timesSorted}`);
-console.log(`Times sorting attempted: ${timesSorted + timesSortingAttempted}`);
-var end = new Date().getTime();
-var time = end - start;
-console.log('Execution time: ' + time);
+
+//Run it here
+
+makeListsForThisSort();
+for (i = 0; i < lists.numberOfLists; i++) {
+    var thisList = arrayOfLists[i];
+    //arrayOfLists[i]
+    //console.log()
+    selectionSort(thisList)
+}
---
+++ expermients.js
@@ -0,0 +1,16 @@
+var shiz = [];
+
+var first = {};
+var second = {};
+
+first.name = "foo";
+first.number = 1;
+second.name = "bar";
+second.number = 2;
+
+shiz.push(first.name, first.number)
+shiz.push(second.name, second.number)
+
+console.log(shiz)
+
+console.log(first.name)
