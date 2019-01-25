a0658bbff69ff52e5f890cdcd579339500e00ac9
Andrew Rees <andrewtrevor.rees@gmail.com>
2019-01-25 12:09:26 +0000

    PoC

--- Dictionaries and Sets/proofOfConcept.js
+++ Dictionaries and Sets/proofOfConcept.js
@@ -1,16 +1,5 @@
-//bloom filter
 //http://codekata.com/kata/kata05-bloom-filters/
 //https://www.jasondavies.com/bloomfilter/
-
-/* 
-PLAN
-- fetch the list from https://github.com/first20hours/google-10000-english/blob/master/20k.txt
-- add the list values to a bitmap [map]
--- take a value, give it 3 hash values, add the 3 values to the bitmap (to start, don't do hashes, do something simpler)
-- get a user input and allow someone to search.
--- take the value, do the 3 hashes/things on it, and see if all of them are there in the map (map.has())
-- return "its probably there" for all 3, and "it's defo not" is one is missing
-*/
 
 ///////basic encoding - take 1st and last letters and add to bitmap, instead of a hash////////
 
